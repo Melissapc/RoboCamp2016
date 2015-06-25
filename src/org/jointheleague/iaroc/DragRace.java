638054a8fe8db79fa63f350e2b968f46e6c7d3cc
Melissa <wilsongirls@WTS1.local>
2015-06-24 19:52:23 -0700

    This is the Robocamp
    All the code of Iaroc for 2015

--- src/org/jointheleague/iaroc/DragRace.java
+++ src/org/jointheleague/iaroc/DragRace.java
@@ -9,15 +9,16 @@ import org.jointheleague.erik.irobot.IRobotCreateAdapter;
 import org.jointheleague.erik.irobot.IRobotCreateInterface;
 import org.jointheleague.iaroc.sensors.UltraSonicSensors;
 
-public class Brain extends IRobotCreateAdapter {
+public class DragRace extends IRobotCreateAdapter {
     private final Dashboard dashboard;
     public UltraSonicSensors sonar;
 
-    public Brain(IOIO ioio, IRobotCreateInterface create, Dashboard dashboard)
+    public DragRace(IOIO ioio, IRobotCreateInterface create, Dashboard dashboard)
             throws ConnectionLostException {
         super(create);
         sonar = new UltraSonicSensors(ioio, dashboard);
         this.dashboard = dashboard;
+
     }
 
     /* This method is executed when the robot first starts up. */
@@ -26,42 +27,29 @@ public class Brain extends IRobotCreateAdapter {
         //what would you like me to do, Clever Human?
 
 
+
     }
 
     /* This method is called repeatedly. */
+    //dragrace
     public void loop() throws ConnectionLostException, InterruptedException {
+        driveDirect(500, 500);
+        SystemClock.sleep(5000);
+        dashboard.log("Test code to see if it goes in loop");
         sonar.read();
         dashboard.log(sonar.getRightDistance() + "   " + sonar.getFrontDistance() + "   " + sonar.getLeftDistance());
         readSensors(SENSORS_BUMPS_AND_WHEEL_DROPS);
-        driveDirect(500,500);
-        readSensors(SENSORS_BUMPS_AND_WHEEL_DROPS);
-        if (isBumpLeft()) {
-            driveDirect(-350, -350);
-            SystemClock.sleep(800);
-            driveDirect(200, 0);
-            SystemClock.sleep(1000);
 
-        }
 
         readSensors(SENSORS_BUMPS_AND_WHEEL_DROPS);
         if (isBumpRight()) {
-            driveDirect(-350, -350);
-            SystemClock.sleep(750);
+            driveDirect(-200, -200);
+            SystemClock.sleep(500);
             driveDirect(0, 340);
-            SystemClock.sleep(1000);
-        }
-        readSensors(SENSORS_BUMPS_AND_WHEEL_DROPS);
-        if (isBumpRight() && isBumpLeft()) {
-
-            driveDirect(300, 300);
-            SystemClock.sleep(1000);
-            driveDirect(0, 300);
             SystemClock.sleep(1000);
 
         }
 
 
     }
-
-
 }
